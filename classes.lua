0d288ade7ff16509c624a45fd29de2dbb2c1801f
ASUS <voronckov.danila@yandex.ru>
2020-05-07 23:06:48 +0300

    Реализация классов в Lua; декларации общих функций и для генераций объектов карты

---
+++ classes.lua
@@ -0,0 +1,77 @@
+do
+    ---@class ClassType
+    ---@class ObjectType
+    local classGeneral = {} ---@type ClassType
+
+    ---@return table
+    function classGeneral:new()
+        local object = {class=tostring(self)} ---@type ObjectType
+        return object
+    end
+
+    ---@param object ObjectType
+    function classGeneral:instantiate(object)
+        object.oldClass = object.class
+        object.class = tostring(self)
+    end
+
+    ---@param inheritedClass ClassType
+    ---@return ClassType
+    function inheritedClass(inheritedClass)
+        local class = setmetatable({},{__index = inheritedClass})
+        class.old = setmetatable({},{__index = inheritedClass})
+        return class
+    end
+
+    ---@param extendedClass ClassType
+    ---@return ClassType
+    function newClass(extendedClass)
+        local classExt = extendedClass and extendedClass or classGeneral
+        local class = inheritedClass(classExt)
+        return class
+    end
+
+    ---@param object ObjectType
+    ---@param class ClassType
+    ---@return boolean
+    function instanceof(object,class)
+        return object.class == tostring(class)
+    end
+end
+
+-- ---@class Class:ClassType
+-- ---@class ClassObject:ObjectType
+-- local class = newClass() ---@type Class
+-- function class:new()
+--     local object = self.old:new() ---@type ClassObject
+--     object.field = 10
+--     return object
+-- end
+
+-- local object = class:new() 
+-- print(object,object.class)
+
+-- ---@class Class2:ClassType
+-- ---@class Class2Object:ObjectType
+-- local class2 = newClass() ---@type Class2
+-- function class2:new()
+--     local object = self.old:new() ---@type Class2Object
+--     object.field2 = 30
+--     return object
+-- end
+
+-- local object2 = class2:new()
+-- print(object2,object2.class)
+
+-- ---@class Class3:Class
+-- ---@class Class3Object:ClassObject
+-- local class3 = newClass(class) ---@type Class3
+-- function class3:new()
+--     local object = self.old:new() ---@type Class3Object
+--     class3:instantiate(object)
+--     object.field3 = 100
+--     return object
+-- end
+
+-- local object3 = class3:new()
+-- print(object3,object3.class)
---
+++ decl.lua
@@ -0,0 +1,251 @@
+---@param func function
+---@return boolean
+function compiletime(func) end
+
+local _FourCC = FourCC
+---@param id string
+---@return number
+function FourCC(id) return _FourCC(id) end
+
+---@class WarObject:ClassType
+---@class obj_WarObject:ObjectType
+WarObject = newClass() ---@type WarObject
+function WarObject:new()
+    local self = self
+
+    local object = self.old:new() ---@type obj_WarObject
+
+    -- All fields available on this object
+    object.all = {}
+
+    -- This object's id as a string.
+    object.id=''
+
+    -- Parent's id as a string. Undefined if this object has no parent, i.e. 
+    -- it is defined by WC3 data itself.
+    object.parentId='' ---@type string|nil
+
+    -- This object's type.
+    object.type=''
+
+    -- Clones this object, creating an independent instance of it.
+    -- This instance can then be assigned into a `WarObjects` object
+    -- to insert it into the map.
+    ---@return obj_WarObject
+    function object.clone() end
+
+    -- /**
+    --  * Gets a field on this object.
+    --  *
+    --  * Two syntaxes are accepted for the `field` parameter:
+    --  * 1) SLK-like, where fields have a 'proper' name, sometimes postfixed with
+    --  * a number to indicate which level of the field to get/set. Examples include,
+    --  * `Name`, `DataA1`, `DataC10`, etc.
+    --  * 2) Raw, using a four-character rawid, optionally with a `+X` postfix
+    --  * to indicate which level to use, for fields which can have multiple levels.
+    --  * E.g.: `unam`, `xxxx+1`, `xxxx+10`
+    --  *
+    --  * The returned value will be automatically converted to the appropriate type.
+    --  * WC3 has 4 fundamental types for fields:
+    --  * real - standard float value
+    --  * unreal - float value clamped between 0.0 and 1.0
+    --  * int - standard signed integer value
+    --  * string - null-terminated string
+    --  *
+    --  * Setting a field into null/undefined will reset it to its default value.
+    --  */
+    ---@param field string
+    ---@return string|number
+    function object.getField(field) end
+
+    -- /**
+    --  * Sets a field on this object.
+    --  *
+    --  * See `getField` for field name syntax.
+    --  *
+    --  * Setting a field into null/undefined will reset it to its default value.
+    --  */
+    ---@param field string
+    ---@param value string|number
+    function object.setField(field,value) end
+
+    return object
+end
+
+---@class WarObjects:ClassType
+---@class obj_WarObjects:ObjectType
+WarObjects = newClass() ---@type WarObjects
+function WarObjects:new()
+    local object = self.old:new() ---@type obj_WarObjects
+
+    -- /** All WC3 objects of this type in the map. Meant for iteration. */
+    object.all = {}
+
+    -- /** Extension string for this WC3 object type. */
+    object.ext = ''
+
+    -- /** Type string for this WC3 object type. */
+    object.typestr = ''
+
+    -- /** Marker whether any object data here has been modified since loading. Used by Ceres to prevent writing out unmodified object stores. */
+    object.isDirty = false
+
+    -- /**
+    -- * Reads WC3 objects from the supplied string into this object.
+    -- * The expected format is that of WC3 object data files, which is the same for all object data types.
+    -- */
+    ---@param data string
+    function object.readFromString(data) end
+
+    -- /**
+    -- * Writes the WC3 objects contained in this object out into the string.
+    -- * The resulting format is that of WC3 object data files, which is the same for all object data types.
+    -- */
+    ---@return string
+    function object.writeToString() end
+
+    -- /**
+    -- * Gets a reference to a WC3 object inside this storage.
+    -- *
+    -- * Index is a rawid. Upon getting an object, you get a
+    -- * reference which you can use to mutate it.
+    -- *
+    -- * For example:
+    -- *
+    -- * // getting a reference to an object
+    -- * let myFoo = currentMap.objects.units['hfoo'] as WarObject
+    -- * // this will mutate the 'hfoo' unit
+    -- * myFoo['Name'] = "Crab"
+    -- * // this will create a clone of the "Crab" 'hfoo' with the id 'xxxx'
+    -- * currentMap.objects.units['xxxx'] = myFoo
+    -- *
+    -- * // this will get a clone instead, which won't modify the original object
+    -- * let myPea = (currentMap.objects.units['hpea'] as WarObject).clone()
+    -- * // this will only modify the clone now
+    -- * myPea['Name'] = "Arnold"
+    -- * currentMap.objects.units['yyyy'] = myPea
+    -- */
+    ---@param rawid string
+    ---@return obj_WarObject
+    function object.getObject(rawid) end
+
+    -- /**
+    -- * Sets a WC3 object into this storage.
+    -- *
+    -- * Index is a rawid.
+    -- *
+    -- * When setting an object, Ceres will clone the target
+    -- * and set the clone's id to that specified here.
+    -- *
+    -- * Setting an object into null/undefined will reset it to defaults for a stock
+    -- * object, and delete it for a custom object.
+    -- *
+    -- * See `getObject` fore example usage.
+    -- */
+    ---@param rawid string
+    ---@param object obj_WarObject
+    function object.setObject(rawid,object) end
+
+    return object
+end
+
+-- Encapsulates all objects of all types in a map.
+---@class WarMapObjects:ClassType
+---@class obj_WarMapObjects:ObjectType
+WarMapObjects = newClass() ---@type WarMapObjects
+function WarMapObjects:new()
+    local object = self.old:new() ---@type obj_WarMapObjects
+    object.ability = {} ---@type obj_WarObject
+    object.item = {} ---@type obj_WarObject
+    object.unit = {} ---@type obj_WarObject
+    object.destructable = {} ---@type obj_WarObject
+    object.doodad = {} ---@type obj_WarObject
+    object.buff = {} ---@type obj_WarObject
+    object.upgrade = {} ---@type obj_WarObject
+    return object
+end
+
+-- Encapsulates a WC3 map during the compilation stage.
+---@class WarMap:ClassType
+---@class obj_WarMap:ObjectType
+WarMap = newClass() ---@type WarMap
+function WarMap:new()
+    local object = self.old:new() ---@type obj_WarMap
+    
+    -- /** Reference to all object storage in this map. */
+    object.objects = {} ---@type obj_WarMapObjects
+
+    -- /**
+    -- * Reads a file from the map. Returns the file contents as a
+    -- * string upon success, or false + an error message upon failure.
+    -- *
+    -- * This function will also "see" any files previously added to the map
+    -- * via `addFileString`, `addFileDisk` or `addDir`.
+    -- *
+    -- * @tupleReturn
+    -- */
+    ---@param path string
+    ---@return string|table
+    function object.readFile(path) end
+
+    -- /**
+    -- * Adds a directory to the map, preserving the file hierarcy.
+    -- * Note: the directory will only be written when writeToDir() or writeToMpq() has been called.
+    -- */
+    ---@param path string
+    function object.addDir(path) end
+
+    -- /**
+    -- * Adds a file to the map with the specified content as a string.
+    -- * Note: the file will only be written when writeToDir() or writeToMpq() has been called.
+    -- */
+    ---@param archivePath string
+    ---@param contents string
+    function object.addFileString(archivePath,contents) end
+
+    -- /**
+    -- * Adds a file to the map, reading it from disk at the specified lcoation.
+    -- * Note: the file will only be written when writeToDir() or writeToMpq() has been called.
+    -- */
+    ---@param archivePath string
+    ---@param diskPath string
+    function object.addFileDisk(archivePath,diskPath) end
+
+    -- /**
+    -- * Writes the map and all manually added files to the specified directory.
+    -- */
+    ---@param path string
+    function object.writeToDir(path) end
+
+    -- /**
+    -- * Writes the map and all manually added files to an MPQ archive at the specified path.
+    -- */
+    ---@param path string
+    function object.writeToMpq(path) end
+
+    -- /**
+    -- * Initializes an object storage for the specified extension type.
+    -- * This will read objects already present in the map if appropriate.
+    -- */
+    ---@param ext string
+    function object.initObjectStorage(ext) end
+
+    -- /**
+    -- * Initializes all object storages in the map.
+    -- */
+    function object.initObjects() end
+
+    -- /**
+    -- * Writes out all the object storages to their respective files in the map.
+    -- */
+    ---@param objects obj_WarObjects
+    function object.commitObjectStorage(objects) end
+
+    return object
+end
+
+-- /**
+--  * Currently compiling map, if any.
+--  * @compiletime
+--  */
+currentMap = WarMap:new()
